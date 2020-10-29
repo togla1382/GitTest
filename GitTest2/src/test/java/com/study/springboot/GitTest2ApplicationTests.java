0c862563b2ff4d3985f10e011b37e99d2c3b81f9
Gi7A-00 <Gi7A-00@i7A-78-PC>
2020-10-29 10:59:14 +0900

    초기버젼입니다.

---
+++ GitTest2/src/test/java/com/study/springboot/GitTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
